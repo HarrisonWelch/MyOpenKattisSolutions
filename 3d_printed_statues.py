0470fe602a992162b230ba7bb6de028630f3114f
HarrisonWelch <hwelch1@my.apsu.edu>
2018-09-05 12:41:55 -0500

    attempted '3d printed statues'; solved 'two stones'

---
+++ 3d_printed_statues.py
@@ -0,0 +1,21 @@
+# 3d_printed_statues.py
+
+n = int(input())
+
+max_printers = n // 2 # floored division
+
+minimum_days = 10e100
+candidate_days
+
+printer_build_days = 2 ** (i-1)
+
+for i in range (max_printers):
+  max_potential = i*n
+  stat_build_days = 0
+  stat_built = 0
+
+  # while stat_build_days
+  candidate_days = printer_build_days + stat_build_days
+
+  if candidate_days < :
+    minimum_days = candidate_days
